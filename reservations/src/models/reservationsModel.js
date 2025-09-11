f830252bd8b71087bae7d29abffe1e222eb600e1
vamphook972 <vamphook972@gmail.com>
2025-09-11 17:43:50 -0500

    Add reservation creation functionality: implemented user and room verification, availability checks, and cost calculation in the reservations controller; updated model to include createReservation method.

--- reservations/src/models/reservationsModel.js
+++ reservations/src/models/reservationsModel.js
@@ -17,7 +17,23 @@ async function getReservationById(id) {
   return result[0];
 }
 
+async function createReservation(reservation) {
+  const user = reservation.user;
+  const id_hotel = reservation.id_hotel;
+  const occupants_number = reservation.occupants_number;
+  const id_room = reservation.id_room;
+  const start_date = reservation.start_date;
+  const end_date = reservation.end_date;
+  const state = reservation.state;
+  const cost = reservation.cost;
+
+  
+  const result = await conection.query('INSERT INTO reservations (user, id_hotel, occupants_number, id_room, start_date, end_date, cost, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [user, id_hotel, occupants_number, id_room, start_date, end_date, state, cost]);
+  return result;
+}
+
 module.exports = {
   getReservations,
-  getReservationById
+  getReservationById,
+  createReservation
 };
