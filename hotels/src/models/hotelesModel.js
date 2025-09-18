69be4686d49cc347a50832afb2fab8cdb3371d8c
vamphook <vamphook972@gmail.com>
2025-09-17 22:35:34 -0500

    Revert "Merge pull request #29 from vamphook972/hotels"
    This reverts commit 89c726795b65144535082ea8eade3ea048c93efe, reversing
    changes made to c5f5aab4b04b32d5fab55fdf5ca260b81c16487e.

--- hotels/src/models/hotelesModel.js
+++ hotels/src/models/hotelesModel.js
@@ -34,26 +34,36 @@ async function traerHotelUsuario(usuario) {
 
 // Crear un nuevo hotel (sin habitaciones)
 async function crearHotel(hotel) {
-  const { usuario, nombre_hotel, pais, ciudad, estado = 'activo'} = hotel;
+  const { usuario, nombre_hotel, pais, ciudad } = hotel;
 
-  const [result] = await connection.query(
+  const [result] = await conection.query(
     `INSERT INTO hoteles 
-     (usuario, nombre_hotel, pais, ciudad, estado)
-     VALUES (?, ?, ?, ?, ?)`,
-    [usuario, nombre_hotel, pais, ciudad, estado]
+     (usuario, nombre_hotel, pais, ciudad)
+     VALUES (?, ?, ?, ?)`,
+    [usuario, nombre_hotel, pais, ciudad]
   );
 
   return result.insertId;
 }
 
 // Actualizar hotel por id
-async function actualizarHotel(id, estado,) {
-  const sql = ` 
-  UPDATE hoteles SET
-     estado = ?
-     WHERE id = ?`;
-  const [result] = await connection.query(sql, [estado, id]);
-  return result;       
+async function actualizarHotel(id, hotel) {
+  const { usuario, nombre_hotel, pais, ciudad } = hotel;
+
+  const [result] = await conection.query(
+    `UPDATE hoteles SET
+     usuario = ?, nombre_hotel = ?, pais = ?, ciudad = ?
+     WHERE id = ?`,
+    [usuario, nombre_hotel, pais, ciudad, id]
+  );
+
+  return result;
+}
+
+// Borrar hotel por id
+async function borrarHotel(id) {
+  const [result] = await conection.query('DELETE FROM hoteles WHERE id = ?', [id]);
+  return result;
 }
 
 
@@ -63,10 +73,7 @@ module.exports = {
   traerHotel,
   crearHotel,
   actualizarHotel,
-<<<<<<< HEAD
-=======
   borrarHotel,
   traerHotelNombre,
   traerHotelUsuario
->>>>>>> c54e3466344fbc6dee4534dbdcca4a5c8932ac68
 };
