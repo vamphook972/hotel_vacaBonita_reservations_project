a426f75a9991db37392d222dc658737d74bc4764
juan_programing <juanestebanparedes592@gmail.com>
2025-11-05 14:46:48 -0500

    initialize database schema and Docker Compose configuration for microservices

---
+++ db/init.sql
@@ -0,0 +1,56 @@
+create database agencia;
+use agencia;
+
+CREATE TABLE users (
+  nombre VARCHAR(25) NOT NULL,
+  tipo_usuario ENUM('cliente', 'admin_hotel', 'admin_agencia') NOT NULL,
+  genero ENUM('masculino', 'femenino') NOT NULL,
+  pais VARCHAR(20) NOT NULL,
+  usuario VARCHAR(30) NOT NULL,
+  password VARCHAR(35) NOT NULL,
+  email VARCHAR(25) NOT NULL UNIQUE,
+  PRIMARY KEY (usuario)
+);
+
+CREATE TABLE hotels (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(100) NOT NULL,
+    nombre_hotel VARCHAR(200) NOT NULL,
+    pais VARCHAR(100) NOT NULL,
+    ciudad_direccion VARCHAR(100) NOT NULL,
+    estado ENUM('activo', 'inactivo') NOT NULL
+);
+
+CREATE TABLE reservations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user VARCHAR(100) NOT NULL,
+    id_hotel INT NOT NULL,
+    occupants_number INT NOT NULL,
+    id_room INT NOT NULL,            
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    state ENUM('pending', 'confirm', 'cancelled', 'finished') NOT NULL,
+    cost DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(100) NOT NULL,
+    id_hotel INT NOT NULL,
+    nombre_hotel VARCHAR(200) NOT NULL,
+    numero_estrellas INT NOT NULL CHECK (numero_estrellas BETWEEN 1 AND 5),
+    comentario TEXT NULL,
+    puntaje_limpieza FLOAT NOT NULL CHECK (puntaje_limpieza BETWEEN 1 AND 10),
+    puntaje_facilidades FLOAT NOT NULL CHECK (puntaje_facilidades BETWEEN 1 AND 10),
+    puntaje_comodidades FLOAT NOT NULL CHECK (puntaje_comodidades BETWEEN 1 AND 10)
+);
+
+CREATE TABLE rooms (
+    id_habitacion INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_habitacion ENUM('estandar', 'deluxe', 'suite') NOT NULL,
+    estado ENUM('libre', 'ocupada') NOT NULL,
+    numero_ocupantes INT NOT NULL,
+    costo_habitacion DECIMAL(10,2) NOT NULL,
+    numero_habitacion INT NOT NULL,
+    id_hotel INT NOT NULL
+);
