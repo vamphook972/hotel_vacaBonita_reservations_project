c80e3d86d0b20cd0308660b20d1642b64218a07f
vamphook972 <vamphook972@gmail.com>
2025-11-13 22:24:51 -0500

    feat: configure Spark with local and master IP settings
    Added configuration steps to the Spark installation script, including setting the local IP and master host in the spark-env.sh file. This enhances the setup process for Spark clusters.

--- scripts/install_spark.sh
+++ scripts/install_spark.sh
@@ -24,3 +24,23 @@ wget https://dlcdn.apache.org/spark/spark-3.5.7/spark-3.5.7-bin-hadoop3.tgz /hom
 tar -xvzf /home/vagrant/spark-3.5.7-bin-hadoop3.tgz
 
 rm spark-3.5.7-bin-hadoop3.tgz
+
+# Configurar Spark
+echo "Configurando Spark..."
+SPARK_DIR="spark-3.5.7-bin-hadoop3"
+LOCAL_IP=$(hostname -I | awk '{print $2}')
+MASTER_IP="192.168.100.3"
+
+cd ${SPARK_DIR}/conf/
+cp spark-env.sh.template spark-env.sh
+
+# Agregar configuración de IP al archivo spark-env.sh
+cat >> spark-env.sh <<EOF
+
+# Configuración de red
+export SPARK_LOCAL_IP=${LOCAL_IP}
+export SPARK_MASTER_HOST=${MASTER_IP}
+EOF
+
+echo "Spark configurado con IP local: ${LOCAL_IP} y Master: ${MASTER_IP}"
+cd -
